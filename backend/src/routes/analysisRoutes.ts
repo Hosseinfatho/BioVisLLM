5b479bf995af22343db18a53ff66fed7339780e9
Hosseinfathollahian <118398599+Hosseinfathollahian@users.noreply.github.com>
2025-04-12 09:17:15 -0500

    first-try for mixing new template to our template

--- backend/src/routes/analysisRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { analyzeRegion } from '../controllers/analysisController';
-
-const router = express.Router();
-
-router.post('/analyze', analyzeRegion);
-
-export default router; 
--- backend/src/routes/imageRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getTile } from '../controllers/imageController';
-
-const router = express.Router();
-
-router.get('/get_tile', getTile);
-
-export default router; 
